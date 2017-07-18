0641786e698e2353e9ea34f0fce365fd11694c2d
Masood Noori <noori2890@users.noreply.github.com>
2017-07-18 06:16:57 -0700

    Create mongodb.rb

---
+++ mongodb.rb
@@ -0,0 +1,33 @@
+#MongoDB
+##########################################################################
+# Cookbook Name:: mongodb
+# Recipe:: install
+#
+#
+#
+case node [:platform]
+when 'redhat', 'centos'
+      file 'etc/yum.repos.d/mongodb-org-3.4.repo' do
+          content'
+[mongodb-org-3.4]
+name=MongoDB Repository
+baseurl=https://repo.mongodb.org/yum/redhat/$releasever/mongodb-org/3.4/x86_64/
+gpgcheck=1
+enabled=1
+gpgkey=https://www.mongodb.org/static/pgp/server-3.4.asc'
+end
+
+#method2
+#mongo_repo = "/etc/yum.repos.d/mongodb-org-3.4.repo"
+#File.open("mongo_repo", a+)
+
+template "mongo_repo" do
+  
+
+package "mongodb" do
+  action :install
+end
+
+service "mongodb" do
+  action [ :enable, :start]
+end
