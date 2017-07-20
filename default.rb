ff44b40f5de799f6d4a28e58bfffc1c9d7fc37ff
Masood Noori <noori2890@users.noreply.github.com>
2017-07-20 03:03:32 -0700

    Update default.rb

--- default.rb
+++ default.rb
@@ -12,6 +12,12 @@ package 'java-1.6.0-openjdk' do
   action:install
 end
 
+#extract and istall of tomcat from binary 
+execute "cd /opt/"
+execute "wget http://apache.claz.org/tomcat/tomcat-8/v8.5.16/bin/apache-tomcat-8.5.16.tar.gz"
+execute "tar -xvf apache-tomcat-8.5.16.tar.gzi"
+
+
 #extract/install of tomcat
 template "/chef-repo/.chef/cookbooks/apachetomcat/templates/exintomcat.erb" do
   source "exintomcat.erb"
