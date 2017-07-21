128c42434033cdb92189b8330bb683a26b2acda0
Masood Noori <noori2890@users.noreply.github.com>
2017-07-20 23:55:03 -0700

    Update and rename default.rb to tomcat.rb

--- tomcat.rb
+++ tomcat.rb
@@ -12,10 +12,27 @@ package 'java-1.6.0-openjdk' do
   action:install
 end
 
+f = open('sample.flv')
+begin
+    http.request_get('/sample.flv') do |resp|
+        resp.read_body do |segment|
+            f.write(segment)
+        end
+    end
+ensure
+    f.close()
+end
+
+
+
 #extract and istall of tomcat from binary 
-execute "cd /opt/"
-execute "wget http://apache.claz.org/tomcat/tomcat-8/v8.5.16/bin/apache-tomcat-8.5.16.tar.gz"
-execute "tar -xvf apache-tomcat-8.5.16.tar.gzi"
+#execute "cd /opt/"
+#execute "wget http://apache.claz.org/tomcat/tomcat-8/v8.5.16/bin/apache-tomcat-8.5.16.tar.gz"
+#execute "tar -xvf apache-tomcat-8.5.16.tar.gzi"
+
+#startup tomcat
+#execute "cd /opt/apache-tomcat-8.5.16/bin"
+#execute "./startup.sh"
 
 
 #extract/install of tomcat
